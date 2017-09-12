c96cbaffef905a8f11cc13cbf8da6134f7b11fed
Daniel Sula <DanielSula@users.noreply.github.com>
2017-09-12 14:15:24 +0300

    Lisaks on esimene jupp PHP koodi

---
+++ Esimene.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+	<meta charset="utf-8">
+	<title>Sula Pesa</title>
+<body>
+	<center>
+	<img src="http://i.imgur.com/6OKJeJ5.png" alt="http://i.imgur.com/6OKJeJ5.png" width="592" height="356">
+	
+	<marquee scrollamount=3 behavior=alternate><p>The stories and information posted here are artistic works of fiction and falsehood.</p></marquee>
+	<marquee scrollamount=3 behavior=alternate direction=right><p>Only a fool would take anything posted here as fact.</p></center></marquee>
+	</center>
+	<left><div style="background-color:black;color:white;padding:10px;">
+	<h3>Autorist</h3>
+	<p><b>Daniel Sula</b> (sündinud 29. septembril 1994 Tallinnas) on eesti kassisymfooniaorkestri juhataja ning helilooja, kes loob klassikalist ja vaimulikku muusikat. Ta on tuntud eelk6ige isikup2rase minimalistliku kompositsioonitehnika, nn <i>kassiv2ntsutamise</i> poolt, mida ta viljeleb alates 2000. aastate esimesest poolest.</p>
+	<p>Kasutab harjumusest Notepad++ tekstiredaktorit</p>
+	</div></left>
+	<br></br>
+	<?php
+		echo "See on esimene jupp PVP abil väljastatud teksti!";
+	?>
+
+<center><footer>Sulstale Productions &copy;</footer></center>
+</body>
+
+
+
+
+
+
+
+</html>
